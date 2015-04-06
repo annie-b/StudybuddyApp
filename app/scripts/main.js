32d6ed5a75896068b45dba8744718688d0de91c1
KBondanza <kelly.bondanza@gmail.com>
2015-04-06 16:55:31 -0400

    added backbone routes, and ajax get request for users

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,16 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+/*global $:true */
+/*global Backbone:true */
+'use strict';
+
+var trace = function(){
+  for(var i = 0; i < arguments.length; i++){
+    console.log(arguments[i]);
+  }
+};
+
+var App = App || {
+  url: 'http://localhost:3000'
+};
+var Category = Category || {};
+var Resource = Resource || {};
+var Flashcard = Flashcard || {};
