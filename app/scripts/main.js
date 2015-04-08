e3ca450aa48e89a3d38b55f0f7a0880d07dee8eb
KBondanza <kelly.bondanza@gmail.com>
2015-04-08 10:57:07 -0400

    added a form and some scripts to update user, not currently working.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,6 +15,7 @@ var App = App || {
 var Category = Category || {};
 var Resource = Resource || {};
 var Flashcard = Flashcard || {};
+var User = User || {};
 
 var Router = Backbone.Router.extend({
   routes: {
@@ -401,6 +402,55 @@ Resource.newResourceForm = function(e,form,router){
    }).fail(function(jqXHR, textStatus, errorThrown){
      trace(App.url + '/#/categories/' + categoryId);
      trace(jqXHR, textStatus, errorThrown);
+   });
+ };
+
+ App.updateUser = function(){
+     $('#container').empty().load('partials/user-form.html',function(response,status,xhr){
+       var $form = $('#user-form');
+       $form.on('submit',function(event){
+         User.updateUserForm(event,$form);
+       });
+     });
+ };
+
+ User.updateUserForm = function(e,form){
+   var locate = window.location.hash;
+   var point = locate.lastIndexOf('/');
+   var userId = parseInt(locate.substring(point+1, locate.length));
+   if(e.preventDefault) e.preventDefault();
+   var name = $(form).find("input[name='user-name']").val();
+   var email = $(form).find("input[name='user-email']").val();
+   User.updateUserParams(name, email, userId);
+ };
+
+ User.updateUserParams = function(name, email, userId){
+   $.ajax({
+     url: App.url + '/users/' + userId,
+     type: 'PATCH',
+     data: {
+       user: {
+         name: name,
+         email: email
+       },
+     },
+     complete: function(jqXHR,textStatus){
+       trace(jqXHR, textStatus, "complete post!!");
+     },
+     success: function(data, textStatus, jqXHR){
+       router.navigate("users",{trigger: true});
+       trace(data,textStatus, jqXHR, "successful post!!");
+     },
+     error: function(jqXHR,error,exception){
+       trace(jqXHR,error,exception);
+     },
+   }).done(function(response){
+     trace(response, "posted user!!");
+   }).fail(function(jqXHR, textStatus, thrownError){
+     trace(jqXHR, textStatus, thrownError);
+     router.navigate("users",{trigger: true});
+   }).always(function(response){
+    trace(response);
    });
  };
 
